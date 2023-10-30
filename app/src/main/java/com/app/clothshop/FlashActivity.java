3dd3545794d304494dc5f13673e3a3e0bac0895b
Thamidu-Premaratna <115412689+Thamidu-Premaratna@users.noreply.github.com>
2023-10-30 21:50:54 +0530

    flash screen

---
+++ app/src/main/java/com/app/clothshop/FlashActivity.java
@@ -0,0 +1,24 @@
+package com.app.clothshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FlashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+
+        Button mBtn = findViewById(R.id.mainBtn);
+        mBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+              startActivity(new Intent(FlashActivity.this,HomeActivity.class));
+            }
+        });
+    }
+}
