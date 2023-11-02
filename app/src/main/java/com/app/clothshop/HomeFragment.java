e33cd704d7f2aada958cf5839500f7a32ff7d1e0
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2023-11-02 15:25:07 +0530

    home screen vertical

--- app/src/main/java/com/app/clothshop/HomeFragment.java
+++ app/src/main/java/com/app/clothshop/HomeFragment.java
@@ -16,9 +16,12 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView homeHorizontalRec;
+    RecyclerView homeHorizontalRec, homeVerticalRec;
     List<HomeHorModel> homeHorModelList;
     HomeHorAdapter homeHorAdapter;
+
+    List<HomeVerModel> homeVerModellist;
+    HomeVerAdapter homeVerAdapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater  inflater, ViewGroup container,
@@ -27,7 +30,9 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeVerticalRec = root.findViewById(R.id.home_ver_rec);
 
+        ///Horizontal RecycleView
         homeHorModelList = new ArrayList<>();
 
         homeHorModelList.add(new HomeHorModel(R.drawable.baseline_house_24,"women"));
@@ -39,6 +44,26 @@ public class HomeFragment extends Fragment {
         homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
         homeHorizontalRec.setHasFixedSize(true);
         homeHorizontalRec.setNestedScrollingEnabled(false);
+
+        ///vertical RecycleView
+        homeHorModelList = new ArrayList<>();
+
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+        homeVerModellist.add(new HomeVerModel(R.drawable.women_1,"cloth1","5.0","25$","Green"));
+
+        homeVerAdapter = new HomeVerAdapter(getActivity(),homeVerModellist);
+        homeVerticalRec.setAdapter(homeVerAdapter);
+        homeVerticalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        homeVerticalRec.setHasFixedSize(true);
+        homeVerticalRec.setNestedScrollingEnabled(false);
+
+
         return root;
     }
 }
